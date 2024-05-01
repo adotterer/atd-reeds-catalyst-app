731f2943fb923f2ae7264b397e7cb3cb0f38daff
Andrew Dotterer <andrew.dotterer@overdose.digital>
2024-05-01 14:21:13 -0400

    Logo work

--- components/store-logo/index.tsx
+++ components/store-logo/index.tsx
@@ -28,19 +28,23 @@ export const StoreLogo = ({ data }: Props) => {
   const { logoV2: logo, storeName } = data;
 
   console.log(data.logoV2, "logo data ------")
+  console.log(storeName, "storeName ------")
 
   if (logo.__typename === 'StoreTextLogo') {
     return <span className="text-2xl font-black">{logo.text}</span>;
   }
 
   return (
-    <BcImage
-      alt={logo.image.altText ? logo.image.altText : storeName}
-      className="max-h-16 object-contain"
-      height={32}
-      priority
-      src={logo.image.url}
-      width={155}
-    />
+    <>
+      <BcImage
+        alt={logo.image.altText ? logo.image.altText : storeName}
+        className="max-h-16 object-contain"
+        height={90}
+        priority
+        src={logo.image.url}
+        width={90}
+      />
+      <span className="text-2xl font-black">{storeName}</span>
+    </>
   );
 };
