6b2e9a01b806d79d23352460b0fe0e81b7b78274
adotterer <adotterer@gmail.com>
2024-06-23 17:37:34 -0400

    copy updates

--- components/hero/index.tsx
+++ components/hero/index.tsx
@@ -14,6 +14,7 @@ import {
 
 import SlideshowBG from './gouger-ai-1.png';
 import SlideshowBG2 from './reed-hero-1.png';
+import SlideshowBG3 from './musicbg-3-extended.png';
 
 export const Hero = () => (
   <Slideshow interval={4000}>
@@ -49,42 +50,47 @@ export const Hero = () => (
     <SlideshowSlide>
         <div className="flex relative h-full">
           <Image
-            alt="Shop Reeds"
+            alt="Oboe lessons"
             className="hover:bg-blend-darken absolute -z-10 object-right object-cover"
             fill
             priority
-            src={SlideshowBG2}
+            src={SlideshowBG3}
           />
           <div className="flex flex-col items-center gap-3 md:gap-7 px-12 py-20 md:py-40 md:items-start">
+            <h4 className="shadow-md max-w-max bg-blue-300 bg-opacity-80 px-2 text-left text-2xl text-black">
+              Looking for online oboe lessons?
+            </h4>
             <h4 className="shadow-md max-w-max bg-blue-300 bg-opacity-80 px-2 text-left text-3xl text-black">
-              Now Accepting 
+              Matt is now accepting new online students!
             </h4>
-            <h4 className="shadow-md max-w-max bg-blue-300 bg-opacity-80 px-2 text-center md:text-left text-3xl text-black">
-              Oboe Reed Pre-Orders*
-            </h4>
-            <h4 className="shadow-md max-w-max bg-blue-300 bg-opacity-80 px-2 text-left text-md text-black">
-              Current wait-time: mid-July
-            </h4>
-            <p className="shadow-md max-w-max max-w-xl bg-blue-300 bg-opacity-80 px-2 text-1xl text-black text-left">
-              
-            </p>
             <Button asChild className="w-fit md:w-fit">
-              <a href="/reeds/">Oboe Reeds</a>
+              <a href="https://www.matthewshippoboe.com/contact">Contact Matt for more information</a>
             </Button>
           </div>
         </div>
     </SlideshowSlide>
       <SlideshowSlide>
         <div className="flexr relative h-full">
+          <Image
+            alt="Oboe Gouging Machine: Kunibert Michel"
+            className="absolute -z-10 object-cover object-right"
+            fill
+            priority
+            sizes="(max-width: 1536px) 100vw, 900px"
+            src={SlideshowBG}
+          />
           <div className="flex flex-col items-center gap-4 px-12 py-36 md:items-start">
             <h2 className="lg:text-4x max-w-max max-w-xl bg-black bg-opacity-50 p-1 pl-2 text-center text-4xl text-white sm:text-4xl md:text-left">
-              Online Lessons with Matt
+              Kunibert Michel
             </h2>
-            <h4 className="shadow-md max-w-max bg-blue-300 bg-opacity-80 px-2 text-left text-md text-black">
-              Interested in oboe lessons?
+            <h4 className="max-w-max bg-black bg-opacity-50 px-2 text-center text-3xl text-white sm:text-3xl md:text-left lg:text-3xl">
+              Oboe Gouging Machine
             </h4>
+            <p className="max-w-max max-w-xl bg-black bg-opacity-50 px-2 text-center text-2xl text-white md:text-left">
+              Used American double-radius gouge.  11 mm bed
+            </p>
             <Button asChild className="w-fit md:w-fit">
-              <a href="https://www.matthewshippoboe.com/contact">Contact me</a>
+              <a href="/used-kunibert-michel-oboe-gouging-machine/">Shop now</a>
             </Button>
           </div>
         </div>
