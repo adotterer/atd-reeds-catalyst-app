c5429f3d2bc91ff4255725ef7fa501e690d31e78
adotterer <adotterer@gmail.com>
2024-06-23 17:19:35 -0400

    .

--- components/hero/index.tsx
+++ components/hero/index.tsx
@@ -28,9 +28,6 @@ export const Hero = () => (
             src={SlideshowBG2}
           />
           <div className="flex flex-col items-center gap-3 md:gap-7 px-12 py-20 md:py-40 md:items-start">
-            {/* <h2 className="shadow-md g:text-5x max-w-max max-w-xl bg-blue-300 bg-opacity-80 p-1 pl-2 text-center text-4xl text-black sm:text-4xl lg:items-center">
-              Good Vibrations
-            </h2> */}
             <h4 className="shadow-md max-w-max bg-blue-300 bg-opacity-80 px-2 text-left text-3xl text-black">
               Now Accepting 
             </h4>
@@ -48,29 +45,46 @@ export const Hero = () => (
             </Button>
           </div>
         </div>
-      </SlideshowSlide>
-      <SlideshowSlide>
-        <div className="flexr relative h-full">
+    </SlideshowSlide>
+    <SlideshowSlide>
+        <div className="flex relative h-full">
           <Image
-            alt="Oboe Gouging Machine: Kunibert Michel"
-            className="absolute -z-10 object-cover object-right"
+            alt="Shop Reeds"
+            className="hover:bg-blend-darken absolute -z-10 object-right object-cover"
             fill
             priority
-            sizes="(max-width: 1536px) 100vw, 900px"
-            src={SlideshowBG}
+            src={SlideshowBG2}
           />
-          <div className="flex flex-col items-center gap-4 px-12 py-36 md:items-start">
-            <h2 className="lg:text-4x max-w-max max-w-xl bg-black bg-opacity-50 p-1 pl-2 text-center text-4xl text-white sm:text-4xl md:text-left">
-              Kunibert Michel
-            </h2>
-            <h4 className="max-w-max bg-black bg-opacity-50 px-2 text-center text-3xl text-white sm:text-3xl md:text-left lg:text-3xl">
-              Oboe Gouging Machine
+          <div className="flex flex-col items-center gap-3 md:gap-7 px-12 py-20 md:py-40 md:items-start">
+            <h4 className="shadow-md max-w-max bg-blue-300 bg-opacity-80 px-2 text-left text-3xl text-black">
+              Now Accepting 
             </h4>
-            <p className="max-w-max max-w-xl bg-black bg-opacity-50 px-2 text-center text-2xl text-white md:text-left">
-              Used American double-radius gouge.  11 mm bed
+            <h4 className="shadow-md max-w-max bg-blue-300 bg-opacity-80 px-2 text-center md:text-left text-3xl text-black">
+              Oboe Reed Pre-Orders*
+            </h4>
+            <h4 className="shadow-md max-w-max bg-blue-300 bg-opacity-80 px-2 text-left text-md text-black">
+              Current wait-time: mid-July
+            </h4>
+            <p className="shadow-md max-w-max max-w-xl bg-blue-300 bg-opacity-80 px-2 text-1xl text-black text-left">
+              
             </p>
             <Button asChild className="w-fit md:w-fit">
-              <a href="/used-kunibert-michel-oboe-gouging-machine/">Shop now</a>
+              <a href="/reeds/">Oboe Reeds</a>
+            </Button>
+          </div>
+        </div>
+    </SlideshowSlide>
+      <SlideshowSlide>
+        <div className="flexr relative h-full">
+          <div className="flex flex-col items-center gap-4 px-12 py-36 md:items-start">
+            <h2 className="lg:text-4x max-w-max max-w-xl bg-black bg-opacity-50 p-1 pl-2 text-center text-4xl text-white sm:text-4xl md:text-left">
+              Online Lessons with Matt
+            </h2>
+            <h4 className="shadow-md max-w-max bg-blue-300 bg-opacity-80 px-2 text-left text-md text-black">
+              Interested in oboe lessons?
+            </h4>
+            <Button asChild className="w-fit md:w-fit">
+              <a href="https://www.matthewshippoboe.com/contact">Contact me</a>
             </Button>
           </div>
         </div>
