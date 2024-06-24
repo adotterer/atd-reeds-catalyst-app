596ff715c5199d18a3528d1e1b656658a5e45eb6
adotterer <adotterer@gmail.com>
2024-06-23 21:20:15 -0400

    .

--- components/category-hero/index.tsx
+++ components/category-hero/index.tsx
@@ -4,6 +4,7 @@ import { FragmentOf, graphql } from '~/client/graphql';
 import { Button } from '../ui/button';
 import React from 'react';
 // import SlideshowBG2 from './reed-box.png';
+
 export const HeaderNavFragment = graphql(`
     fragment HeaderNavFragment on Site {
       categoryTree {
